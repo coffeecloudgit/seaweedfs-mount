a012e374294857bc6a9b16dbfd7efba5d1fbd777
yangpro <y62069061@gmail.com>
2024-09-13 16:05:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## 安装步骤
+
+```shell
+cd /usr/local
+mkdir weed-fs
+cd weed-fs
+
+```
+
+
+
---
+++ fs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+WORKDIR=$(dirname "${BASH_SOURCE-$0}")
+cd $WORKDIR
+WORKDIR=$(pwd)
+
+install(){
+  echo "$WORKDIR"
+
+  cat > /etc/systemd/system/weed-fs.service <<-EOF
+[Unit]
+Description=SeaweedFS Mount
+After=network.target
+After=syslog.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=10
+User=root
+Group=root
+ExecStartPre=-/bin/sleep 5s
+WorkingDirectory=$WORKDIR
+ExecStart=$WORKDIR/weed mount -options=$WORKDIR/fs.conf
+SyslogIdentifier=seaweedfs-mount
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+}
+
+#使用说明，用来提示输入参数
+usage() {
+ echo "Usage: bash fs.sh [install|sync]"
+ exit 1
+}
+
+#根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+ "install")
+ install
+ ;;
+ *)
+ usage
+ ;;
+esac
